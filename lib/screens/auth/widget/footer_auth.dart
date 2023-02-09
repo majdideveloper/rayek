25549a8a9e9558482edf94242ebbfde9789f8937
HanaOuerghemmi <5004@holbertonschool.com>
2023-02-09 18:59:46 +0100

    welcome page

---
+++ lib/screens/auth/widget/footer_auth.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:rayek_v001/utils/utils.dart';
+
+class FooterAuth extends StatelessWidget {
+  final void Function() onTap;
+
+  const FooterAuth({Key? key, required this.onTap}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: RichText(
+        text: TextSpan(
+          text: 'Already Have Account? ',
+          style: smallStyle,
+          children: [
+            TextSpan(
+              text: 'Login',
+              style: H2Style,
+              recognizer: TapGestureRecognizer()..onTap = onTap,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
