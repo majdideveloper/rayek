054023ed20ad3abba582f410255c3936a0d15c5e
HanaOuerghemmi <5004@holbertonschool.com>
2023-02-12 18:46:36 +0100

    it just some thing way to go for the profile

---
+++ lib/screens/profile/widget/covert_image.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:rayek_v001/utils/palette.dart';
+
+class CouvertImage extends StatelessWidget {
+  const CouvertImage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
