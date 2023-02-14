fe02e512eb639aa44da3b2dfc0a46a21d72925f2
majdi aribi <majdi.developer@gmail.com>
2023-02-14 16:20:29 +0100

    UI profile

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -4,7 +4,7 @@ import 'package:flutter/src/widgets/framework.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:rayek_v001/resources/auth_methods.dart';
 import 'package:rayek_v001/screens/auth/auth.dart';
-import 'package:rayek_v001/screens/profile/widget/profile_image.dart';
+
 import 'package:rayek_v001/utils/utils.dart';
 
 import '../profile/profile_screen.dart';
